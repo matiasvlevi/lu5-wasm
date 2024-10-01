95dcbe0b5fb9cee7e6c80c89253649bfe7eb37fe
matiasvlevi <matiasvlevi@gmail.com>
2024-10-01 01:08:00 -0400

    WebAssemblyInstance type

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,3 @@
 export type PlatformFunction = (...args: any) => any;
+
+export type WebAssemblyInstance = (WebAssembly.WebAssemblyInstantiatedSource & { instance: { exports: Record<string, PlatformFunction> } });
