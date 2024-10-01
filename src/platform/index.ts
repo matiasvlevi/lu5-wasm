288deb96740a0ccda15a2a914a19cdc491e8cc0f
matiasvlevi <matiasvlevi@gmail.com>
2024-10-01 01:08:50 -0400

    file io not supported warnings

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -2,7 +2,7 @@ import { get_cstr } from "../memory";
 import { LU5 } from "../lu5";
 
 export * from './geometry/2D'
-export * from './geometry/3D'
+export * from './image'
 
 export function lu5_noLoop(this: LU5) {
     this.loop = false;
@@ -26,13 +26,15 @@ export function lu5_getCursorPos(this: LU5, _window: number, mousex_ptr: number,
     return 0;
 }
 
-export function lu5_init_freetype() { 
-    return 0 
-};
+export async function lu5_read_file(this: LU5, file_path_ptr: number, file_size_ptr: number, err_ptr: number) {
+    console.warn('File I/O is not supported in lu5-wasm yet');
+    return 0;
+}
 
-export function lu5_close_fonts() { 
-    return 0 
-};
+export function lu5_write_file() {
+    console.warn('File I/O is not supported in lu5-wasm yet');
+    return 0;
+}
 
 export function lu5_load_and_add_font(this: LU5, _lu5: number, _font_ptrptr: number, name_ptr: number) {
     console.warn(`'lu5_load_and_add_font' is not yet implemented...`);
@@ -44,8 +46,6 @@ export function lu5_set_font(this: LU5, fontname_ptr: number) {
 
     return 0;
 }
-
-export function lu5_load_default_font() { return 0 };
 
 export function lu5_time_seed() {
     return Math.floor(Math.random() * 1000000);
