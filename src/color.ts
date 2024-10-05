3d2f3351c2f1d4a50e52f23b9d457a547b488d89
matiasvlevi <matiasvlevi@gmail.com>
2024-10-05 02:03:39 -0400

    return transparent colors

--- src/color.ts
+++ src/color.ts
@@ -2,9 +2,11 @@ import { LU5 } from "./lu5";
 
 export function colorToRGBA(this:LU5, ptr: number) {
     this.refreshMemory();
-    if (!this.view) return '#000';
+    if (!this.view) return '#00000000';
+
     const alpha = this.view.getUint8(ptr);
-    if (alpha == 0) return '#000';
+    if (alpha == 0) return '#00000000';
+    
     const color = '#' +
         this.view.getUint8(ptr + 3).toString(16).padStart(2, '0')+
         this.view.getUint8(ptr + 2).toString(16).padStart(2, '0')+
