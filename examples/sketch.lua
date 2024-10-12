b25eab3e67665b59aab4d0fa14884ad2f5e1a437
matiasvlevi <matiasvlevi@gmail.com>
2024-10-12 04:22:01 -0400

    sketch example

--- examples/sketch.lua
+++ examples/sketch.lua
@@ -1,1 +1,10 @@
-local x = 
+function setup()
+    createWindow(200, 200);
+    textAlign(CENTER);
+end
+
+function draw()
+    background('orange');
+
+    text('From ./sketch.lua', width/2, height/2)
+end
