810888e7aa4882cb25cac2ad52c9a300b80a0964
matiasvlevi <matiasvlevi@gmail.com>
2024-10-18 01:18:36 -0400

    esm and umd builds

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const baseConfig = {
-    entry: {
-        'lu5-wasm': './src/lu5-wasm.ts',
-        'lu5-console': './src/console.ts'
-    },
     resolve: {
         extensions: ['.ts', '.js'],
     },
@@ -20,36 +16,74 @@ const baseConfig = {
         ],
     },
     plugins: [
+        // This will make foobar available globally, so secondary can use it
         new webpack.ProvidePlugin({
-          'get_or_create_by_id': path.resolve(__dirname, 'src/common/dom.ts'),
+            'get_or_create_by_id': path.resolve(__dirname, 'src/common/dom.ts'),
         }),
-      ],
+    ],
     mode: 'production'
 };
 
+const lu5_wasm = {
+    entry: {
+        'lu5-wasm': './src/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        library: "lu5",
+        libraryTarget: "umd"
+    }
+}
+
+const lu5_wasm_lib = {
+    entry: {
+        'lu5-wasm-lib': './src/lib.ts'
+    },
+
+    experiments: {
+        outputModule: true, // Enables output as an ES module
+    },
+    externalsType: "module",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'module',
+        chunkFormat: 'module',
+        globalObject: 'window',
+    }
+}
+
 // Minified configuration
-const minified = {
-    ...baseConfig,
+const minified = (c) => ({
+    ...c,
     output: {
         filename: '[name].min.js',
-        path: path.resolve(__dirname, 'dist')
+        ...c.output
     },
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
+    }
+});
 
+const non_minified = (c) => {
+    return {
+        ...c,
+        output: {
+            filename: '[name].js',
+            ...c.output
+        },
+        optimization: {
+            minimize: false
+        }
     }
 };
 
-const non_minified = {
-    ...baseConfig,
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-        minimize: false
-    }
-};
+module.exports = [
+    // UMD Module
+    non_minified({ ...baseConfig, ...lu5_wasm }),
+    minified({ ...baseConfig, ...lu5_wasm }),
 
-module.exports = [non_minified, minified];
+    // ES Module
+    non_minified({ ...baseConfig, ...lu5_wasm_lib }),
+    minified({ ...baseConfig, ...lu5_wasm_lib })
+];
