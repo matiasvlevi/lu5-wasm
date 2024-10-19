5154892fa93a81f927d7b5a564758471bc0be6dc
matiasvlevi <matiasvlevi@gmail.com>
2024-10-18 22:03:21 -0400

    removed dev option

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,2 @@
 
-export const DEV = false;
-export const LU5_WASM_CDN = DEV ? 'http://127.0.0.1:5500/bin/wasm/lu5.wasm' : `https://unpkg.com/lu5-wasm@latest/dist/lu5.wasm`;
+export const LU5_WASM_CDN = `https://unpkg.com/lu5-wasm@latest/dist/lu5.wasm`;
